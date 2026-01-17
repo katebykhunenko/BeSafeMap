be9d8c26d38fb4e1c2edac20ec582c368bdb7a58
Vitalij35 <kostenkovitalij35@gmail.com>
2026-01-17 11:40:31 +0200

    WIP #1

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,15 @@
 
 #define LED_PIN 4       // GPIO4 (D2 на NodeMCU)
 #define LED_COUNT 12
-#define LED_TYPE WS2815
-#define COLOR_ORDER RGB
 
 CRGB leds[LED_COUNT];
 
 void setup() {
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_COUNT);
+  FastLED.addLeds<WS2815, LED_PIN, GRB>(leds, LED_COUNT);
   FastLED.setBrightness(100);
-  delay(100); // даємо ESP стартувати
 
-  // Усі LED червоним
   for (int i = 0; i < LED_COUNT; i++) {
-    leds[i] = CRGB::Red;
+    leds[i].setHue(i * 255 / LED_COUNT);
   }
   FastLED.show();
 }
