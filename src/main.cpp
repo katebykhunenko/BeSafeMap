2e6700fcc01bde7ac767b24b87175464d0e40567
Vitalij35 <kostenkovitalij35@gmail.com>
2026-01-17 16:43:02 +0200

    still doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,18 @@
 
 #include <FastLED.h>
 
-#define LED_PIN 4       // GPIO4 (D2 на NodeMCU)
+#define LED_PIN 2       // GPIO2 (D4 на NodeMCU)
 #define LED_COUNT 12
 
 CRGB leds[LED_COUNT];
 
 void setup() {
   FastLED.addLeds<WS2815, LED_PIN, GRB>(leds, LED_COUNT);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(50); 
 
   for (int i = 0; i < LED_COUNT; i++) {
-    leds[i].setHue(i * 255 / LED_COUNT);
+    //leds[i].setHue(i * 255 / LED_COUNT);
+    leds[i].setRGB(255, 255, 0);
   }
   FastLED.show();
 }
